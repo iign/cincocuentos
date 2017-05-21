8066214b09a0c05acb3a06f57e0a7335c8e7ab48
Ignacio Toledo <ignacio@ign.uy>
2017-05-21 16:40:59 -0300

    Added site

---
+++ site/src/controllers.php
@@ -0,0 +1,52 @@
+<?php
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+//Request::setTrustedProxies(array('127.0.0.1'));
+
+// Homepage
+$app->get('/', function () use ($app) {
+    return $app['twig']->render('index.html.twig', array());
+})->bind('homepage');
+
+// Books -- old, delete.
+// $app->get('/almohadon-de-plumas/{page}', function ($page) use ($app) {
+//     return $app['twig']->render('books/almohadon_page_1.html.twig', array('title' => 'Almohadón de plumas'));
+// })->bind('almohadon');
+
+// Books
+$app->get('/{slug}/{page}/', function ($slug, $page) use ($app) {
+    $fi = new FilesystemIterator("../templates/books/$slug", FilesystemIterator::SKIP_DOTS);
+    $totalPages = iterator_count($fi) - 1;
+
+    if (!$page) {
+        return $app['twig']->render("books/$slug/intro.html.twig", array('totalPages' => $totalPages));
+    }
+    else{
+        return $app['twig']->render("books/$slug/page_$page.html.twig",
+        array('page' => $page, 'totalPages' => $totalPages, 'slug' => $slug));
+    }
+})
+->value('page', null) // Default page value
+->bind('book'); // Route name
+
+// Errors
+$app->error(function (\Exception $e, Request $request, $code) use ($app) {
+    if ($app['debug']) {
+        return;
+    }
+
+    // 404.html, or 40x.html, or 4xx.html, or error.html
+    $templates = array(
+        'errors/'.$code.'.html.twig',
+        'errors/'.substr($code, 0, 2).'x.html.twig',
+        'errors/'.substr($code, 0, 1).'xx.html.twig',
+        'errors/default.html.twig',
+    );
+
+    return new Response($app['twig']->resolveTemplate($templates)->render(array('code' => $code)), $code);
+});
---
+++ site/web/js/app.js
@@ -0,0 +1,68 @@
+/* global $ */
+function markVowels (word) {
+  return word.replace(/(a|e|i|o|u)/ig, '<span class="vowel">$1</span>')
+}
+
+var words
+
+function parseVowels () {
+  words = $('.book').html().split(' ')
+  var text = ''
+
+  for (var i = 0; i < words.length; i++) {
+    if (words[i].indexOf('mark') === -1 && words[i].indexOf('data-trigger') === -1) {
+      // console.log(words[i], '--update')
+      text += markVowels(words[i])
+    } else {
+      // console.log(words[i], '--skip')
+      text += words[i]
+    }
+    text += ' '
+  }
+
+  $('.book').html(text)
+
+  // Parse links
+  $('mark').each(function () {
+    console.log($(this).html())
+    // console.log(markVowels($(this).html()))
+
+    $(this).addClass('test')
+
+    var newWord = markVowels($(this).html())
+
+    $(this).html(newWord).addClass('test')
+  })
+}
+
+;(function () {
+  if ($('body').hasClass('page-read')) {
+    parseVowels()
+  }
+
+  $('mark').on('click', function () {
+    var trigger = $(this).data('trigger')
+    console.log('trigger', trigger)
+    $('.help-box').removeClass('help-box--show')
+    $(trigger).addClass('help-box--show')
+  })
+
+  $('.js-close-help').on('click', function () {
+    $('.help-box').removeClass('help-box--show')
+  })
+
+  // Cambiar tamaño de letra
+  $('.js-btn-size').on('click', function () {
+    $('body').toggleClass('size')
+  })
+
+  // Cambiar contraste
+  $('.js-btn-contrast').on('click', function () {
+    $('body').toggleClass('contrast')
+  })
+
+  // Diferenciar vocales
+  $('.js-btn-vowels').on('click', function () {
+    $('body').toggleClass('vowels')
+  })
+})()
